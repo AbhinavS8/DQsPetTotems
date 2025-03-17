8e7bbd8bb88d128aac174eefb00bb4b98b81b47a
AbhinavS8 <abhinav.subr@gmail.com>
2025-03-17 23:25:40 +0530

    pet charm item added with no functionality

--- src/main/java/net/dominosq/dqpetrespawn/item/ModItems.java
+++ src/main/java/net/dominosq/dqpetrespawn/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.dominosq.dqpetrespawn.item;
 
 import net.dominosq.dqpetrespawn.DQPetRespawn;
+import net.dominosq.dqpetrespawn.item.custom.PetCharmItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -9,8 +10,8 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DQPetRespawn.MODID);
 
-//    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
-//            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> PET_CHARM_ITEM = ITEMS.register("pet_charm_item",
+            () -> new PetCharmItem(new Item.Properties().stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
