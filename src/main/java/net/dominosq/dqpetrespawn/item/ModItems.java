ab42a3c1789c47cdbb3dc42793dff5a5eff843f9
AbhinavS8 <abhinav.subr@gmail.com>
2026-01-22 13:58:12 +0530

    added recipe for totem

--- src/main/java/net/dominosq/dqpetrespawn/item/ModItems.java
+++ src/main/java/net/dominosq/dqpetrespawn/item/ModItems.java
@@ -3,6 +3,7 @@ package net.dominosq.dqpetrespawn.item;
 import net.dominosq.dqpetrespawn.DQPetRespawn;
 import net.dominosq.dqpetrespawn.item.custom.PetTotemItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -11,7 +12,9 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DQPetRespawn.MODID);
 
     public static final DeferredItem<Item> PET_TOTEM_ITEM = ITEMS.register("pet_totem_item",
-            () -> new PetTotemItem(new Item.Properties().stacksTo(1)));
+            () -> new PetTotemItem(new Item.Properties()
+                    .stacksTo(4)
+                    .rarity(Rarity.UNCOMMON)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
